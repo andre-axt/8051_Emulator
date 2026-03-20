ab7ca36eb8015875a82a3e6dd3cd2e391218c070
andre-axt <andreaxtbachtold@gmail.com>
2026-03-19 21:04:25 -0300

    edit - enhance get_bit with error checking for bit_addr
    Add error handling for bit address in get_bit function.

--- src/core/opcodes.c
+++ src/core/opcodes.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 
 static uint8_t get_bit(cpu_t *cpu, uint8_t bit_addr) {
-	
+	if (bit_addr > 0x7F){
+		printf("error: bit address");
+		return 1;
+	} 
+	return uint8_t bit = cpu->mem->ram.bytes[bit_addr];
 
+}
+
+void instr_setb(cpu_t *cpu){
+	
 }
