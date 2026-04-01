9f1ef510f9de3dacef710af8fc1fb8b5917bd0d8
andre-axt <andreaxtbachtold@gmail.com>
2026-04-01 20:36:54 -0300

    add - interruptions.h for interrupt handling
    Add header file for interrupt definitions and structure.

---
+++ src/peripherals/interruptions.h
@@ -0,0 +1,17 @@
+#ifndef INTERRUPTIONS_H
+#define INTERRUPTIONS_H
+#include <stdint.h>
+
+#define INT0 0x03
+#define TIMER0 0x0B
+#define INT1 0x13
+#define TIMER1 0x1B
+#define SERIAL 0x23
+
+typedef struct {
+  int8_t INT0;
+  int8_t Timer0;
+  int8_t INT1;
+  int8_t Timer1;
+  int8_t Serial;
+} Interruptions_t
