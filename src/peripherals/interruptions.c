22b65de582a5bb5c9329a02a9d4e525995b61ab7
andre-axt <andreaxtbachtold@gmail.com>
2026-05-27 17:26:50 -0300

    Add - set_int function to manage interruptions

--- src/peripherals/interruptions.c
+++ src/peripherals/interruptions.c
@@ -8,3 +8,34 @@ void init_int(Interruptions *interruptions){
   interruptions->Serial = 1;
   
 }
+
+inline void set_int(Interruptions_t *interruptions, int8_t handler) {
+  if(handler == 0) return;
+  switch(handler) {
+    case 1{
+      interruptions->INT0 = 0;
+      return;
+    }
+    case 2{
+      interruptions->Timer0 = 0;
+      return;
+    }
+    case 3{
+      interruptions->INT1 = 0;
+      return;
+    }
+    case 4{
+      interruptions->Timer1 = 0;
+      return;
+    }
+    case 5{
+      interruptions->Serial = 0;
+      return;
+    }
+    case 6{
+      interruptions->EA = 0;
+      return;
+    }
+  }
+  return;
+}
