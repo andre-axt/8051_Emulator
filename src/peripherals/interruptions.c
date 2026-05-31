e9a06418c6f08d662cca6852fe779726ffd52a3f
andre-axt <andreaxtbachtold@gmail.com>
2026-05-30 23:07:56 -0300

    Add - get_priority function

--- src/peripherals/interruptions.c
+++ src/peripherals/interruptions.c
@@ -9,6 +9,44 @@ void init_int(Interruptions *interruptions){
   
 }
 
+int8_t get_priority(uint8_t *IP, int8_t handler) {
+  if(handler == 0) return -1;
+  switch(handler) {
+    case 1 {
+      if(*IP && P_PX0) {
+        return 1;
+      }	
+      return 0;
+    }
+    case 2 {
+      if(*IP && P_PT0) {
+        return 1;
+      }	
+      return 0;
+    }
+    case 3 {
+      if(*IP && P_PX1) {
+        return 1;
+      }	
+      return 0;
+    }
+    case 4 {
+      if(*IP && P_PT1) {
+        return 1;
+      }	
+      return 0;
+    }
+    case 5 {
+      if(*IP && P_PS) {
+        return 1;
+      }	
+      return 0;
+    }
+  }
+
+  return -1;
+}
+
 inline void lock_int(uint16_t *pc, int8_t handler) {
   if(handler == 0) return;
   switch(handler) {
