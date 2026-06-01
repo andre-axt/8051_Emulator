ca9748746985e8d11ac02222a206f8f8fcf7d234
andre-axt <andreaxtbachtold@gmail.com>
2026-06-01 16:32:43 -0300

    Refactor - memory functions to use Memory_t type

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -4,12 +4,12 @@
 #include <string.h>
 #include <stdint.h>
 
-int memory_init(memory_t *mem){
+int memory_init(Memory_t *mem){
 	if (mem == NULL) return 1;
 
-	memset(mem, 0, sizeof(memory_t));
+	memset(mem, 0, sizeof(Memory_t));
 
-	memset(mem->ram, 0, sizeof(internal_ram_t));
+	memset(mem->ram, 0, sizeof(Internal_ram_t));
 	memset(mem->internal_rom, 0, INTERNAL_ROM_SIZE);
 	mem->ram.current_bank = 0;
 
@@ -37,14 +37,13 @@ int memory_init(memory_t *mem){
 	mem->sfr.P2 = 0xFF;
 	mem->sfr.P3 = 0xFF;
 
-	mem->ram.current_bank = 0;
-
 	mem->timers = NULL;
+	mem->ram.current_bank = 0;
 
 	return 0;
 }
 
-uint8_t memory_read_code (memory_t *mem, uint16_t address) {
+uint8_t memory_read_code (Memory_t *mem, uint16_t address) {
 	if(address < INTERNAL_ROM_SIZE){
 		return mem->internal_rom[address];
 	}
@@ -52,7 +51,7 @@ uint8_t memory_read_code (memory_t *mem, uint16_t address) {
 
 }
 
-static uint8_t memory_read_sfr(memory_t *mem, uint8_t address) {
+static uint8_t memory_read_sfr(Memory_t *mem, uint8_t address) {
 	switch (address) {
         
 		case 0x80: return mem->sfr.P0;
@@ -83,7 +82,7 @@ static uint8_t memory_read_sfr(memory_t *mem, uint8_t address) {
     	}
 }
 
-uint8_t memory_read_data (memory_t *mem, uint8_t address) {
+uint8_t memory_read_data (Memory_t *mem, uint8_t address) {
 	if (address < 0x80) {
 		return mem->ram.bytes[address];
 	}	
@@ -93,7 +92,7 @@ uint8_t memory_read_data (memory_t *mem, uint8_t address) {
 
 }
 
-static void memory_write_sfr (memory_t *mem, uint8_t address, uint8_t value) {
+static void memory_write_sfr (Memory_t *mem, uint8_t address, uint8_t value) {
 	switch (address) {
         	case 0x80: mem->sfr.P0 = value; break;
         	case 0x90: mem->sfr.P1 = value; break;
@@ -146,7 +145,7 @@ static void memory_write_sfr (memory_t *mem, uint8_t address, uint8_t value) {
 
 }
 
-void memory_write_data (memory_t *mem, uint8_t address, uint8_t value) {
+void memory_write_data (Memory_t *mem, uint8_t address, uint8_t value) {
 	if (address < 0x80) {
 		mem->ram.bytes[address] = value;
 	}
@@ -157,7 +156,7 @@ void memory_write_data (memory_t *mem, uint8_t address, uint8_t value) {
 
 }
 
-int memory_load_program (memory_t *mem, const uint8_t *program, const uint16_t size, const uint16_t start_address) {
+int memory_load_program (Memory_t *mem, const uint8_t *program, const uint16_t size, const uint16_t start_address) {
 	if (mem == NULL || program == NULL || size == 0) {
 		return -1;
 	
@@ -172,7 +171,7 @@ int memory_load_program (memory_t *mem, const uint8_t *program, const uint16_t s
 
 }
 
-void update_sfr_from_timer0(memory_t *mem) {
+void update_sfr_from_timer0(Memory_t *mem) {
 	
 	if (mem->timers == NULL) return;
 
@@ -183,7 +182,7 @@ void update_sfr_from_timer0(memory_t *mem) {
 
 }
 
-void update_sfr_from_timer1(memory_t *mem) {
+void update_sfr_from_timer1(Memory_t *mem) {
 	
 	if (mem->timers == NULL) return;
 
