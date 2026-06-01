e7ff9e47d6eabae3bad4346af005c98c4c2703ba
andre-axt <andreaxtbachtold@gmail.com>
2026-06-01 16:46:12 -0300

    Rename - cpu_t to Cpu_t and modify includes, Remove - other structs in Cpu_t ,  Add - SP pointer from Sfr_t struct
    Refactor CPU structure and update function signatures.

--- src/core/cpu.h
+++ src/core/cpu.h
@@ -2,24 +2,19 @@
 #define CPU_H
 
 #include <stdint.h>
-#include "memory.h"
-#include "timer.h"
-#include "interruptions.h"
 
 typedef struct {
-	memory_t mem;
-	timers_system_t timers;
-	Interruptions_t interruptions;
 	uint16_t PC;
+	uint8_t *SP;
 	uint64_t total_cycles;
 	uint8_t halted;	
 
-} cpu_t;
+} Cpu_t;
 
-void cpu_init(cpu_t *cpu);
-uint8_t fetch_byte(cpu_t *cpu);
-uint16_t fetch_word(cpu_t *cpu);
-void cpu_step(cpu_t *cpu);
-void cpu_run(cpu_t *cpu, uint32_t steps);
+void cpu_init(Cpu_t *cpu);
+uint8_t fetch_byte(Cpu_t *cpu);
+uint16_t fetch_word(Cpu_t *cpu);
+void cpu_step(Cpu_t *cpu);
+void cpu_run(Cpu_t *cpu, uint32_t steps);
 
 #endif
