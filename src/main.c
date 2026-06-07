89c70baee0c19680140eebe3ecafe0f42fc3aef4
andre-axt <andreaxtbachtold@gmail.com>
2026-06-06 22:44:04 -0300

    Implement - memory allocation for 8051 struct
    Initialize 8051 struct and allocate memory for its components, handling errors appropriately.

--- src/main.c
+++ src/main.c
@@ -8,9 +8,38 @@ typedef struct {
 	Timers_system_t *timers;
 	Memory_t *mem;
 	Cpu_t *cpu;
-} 8051_t;
+} 8051_t; // It will stay here until I think of a better place to put this struct.
+
+8051_t *8051;
 
 int main(){
-	// nothing for now
-
+	8051 = malloc(sizeof(8051_t);
+	if (8051 == NULL) return 1;
+	8051->cpu = cpu_init();
+	if (8051->cpu == NULL){
+		free(8051);
+		return 1;
+	}
+	8051->mem = memory_init();
+	if (8051->mem == NULL){
+		free(8051->cpu);
+		free(8051);
+		return 1;
+	} 
+	8051->timers = init_timers();
+	if (8051->timers == NULL){
+		free(8051->cpu);
+		free(8051->mem);
+		free(8051);
+		return 1;
+	} 
+	8051->intpt = init_int();
+	if (8051->inipt == NULL){
+		free(8051->cpu);
+		free(8051->mem);
+		free(8051->timers);
+		free(8051);
+		return 1;
+	} 
+	
 }
