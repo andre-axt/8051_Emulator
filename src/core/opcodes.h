65a54f99616118cca5a004b2ce84d5bac4438574
andre-axt <andreaxtbachtold@gmail.com>
2026-06-23 15:54:56 -0300

    Fix - type casing for Cpu_t and Instrunction_t

--- src/core/opcodes.h
+++ src/core/opcodes.h
@@ -7,38 +7,38 @@ typedef struct {
 	char *mnemonic;
 	uint8_t bytes;
 	uint8_t cycles;
-	void (*execute)(cpu_t *);
+	void (*execute)(Cpu_t *);
 
-} instrunction_t;
+} Instrunction_t;
 
 extern instruction_t opcode_table[256];
 
-void instr_nop(cpu_t *cpu);
-void instr_ljmp(cpu_t *cpu);
-void instr_sjmp(cpu_t *cpu);
-void instr_ajmp(cpu_t *cpu);
-void instr_mov_a_immed(cpu_t *cpu);
-void instr_mov_direct_immed(cpu_t *cpu);
-void instr_mov_a_direct(cpu_t *cpu);
-void instr_mov_direct_a(cpu_t *cpu);
-void instr_add_a_immed(cpu_t *cpu);
-void instr_setb(cpu_t *cpu);
-void instr_clr(cpu_t *cpu);
-void instr_cpl(cpu_t *cpu);
-void instr_jnb(cpu_t *cpu);
-void instr_jb(cpu_t *cpu);
-void instr_jbc(cpu_t *cpu);
-void instr_anl_a_immed(cpu_t *cpu);
-void instr_orl_a_immed(cpu_t *cpu);
-void instr_xrl_a_immed(cpu_t *cpu);
-void instr_inc_a(cpu_t *cpu);
-void instr_dec_a(cpu_t *cpu);
-void instr_push(cpu_t *cpu);
-void instr_pop(cpu_t *cpu);
-void instr_ret(cpu_t *cpu);
-void instr_reti(cpu_t *cpu);
+void instr_nop(Cpu_t *cpu);
+void instr_ljmp(Cpu_t *cpu);
+void instr_sjmp(Cpu_t *cpu);
+void instr_ajmp(Cpu_t *cpu);
+void instr_mov_a_immed(Cpu_t *cpu);
+void instr_mov_direct_immed(Cpu_t *cpu);
+void instr_mov_a_direct(Cpu_t *cpu);
+void instr_mov_direct_a(Cpu_t *cpu);
+void instr_add_a_immed(Cpu_t *cpu);
+void instr_setb(Cpu_t *cpu);
+void instr_clr(Cpu_t *cpu);
+void instr_cpl(Cpu_t *cpu);
+void instr_jnb(Cpu_t *cpu);
+void instr_jb(Cpu_t *cpu);
+void instr_jbc(Cpu_t *cpu);
+void instr_anl_a_immed(Cpu_t *cpu);
+void instr_orl_a_immed(Cpu_t *cpu);
+void instr_xrl_a_immed(Cpu_t *cpu);
+void instr_inc_a(Cpu_t *cpu);
+void instr_dec_a(Cpu_t *cpu);
+void instr_push(Cpu_t *cpu);
+void instr_pop(Cpu_t *cpu);
+void instr_ret(Cpu_t *cpu);
+void instr_reti(Cpu_t *cpu);
 
-extern uint8_t fetch_byte(cpu_t *cpu);
-extern uint16_t fetch_word(cpu_t *cpu);
+extern uint8_t fetch_byte(Cpu_t *cpu);
+extern uint16_t fetch_word(Cpu_t *cpu);
 
 #endif
