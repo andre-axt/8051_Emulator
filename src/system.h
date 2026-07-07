0e497c8e6cb705ab159c0e125c37a92f5e1651ec
andre-axt <andreaxtbachtold@gmail.com>
2026-07-07 14:19:41 -0300

    Add - system.h header file for MCU8051 structure

---
+++ src/system.h
@@ -0,0 +1,16 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include "cpu.h"
+#include "memory.h"
+#include "timer.h"
+#include "interruptions.h"
+
+typedef struct {
+    Cpu_t *cpu;
+    Memory_t *mem;
+    Timers_system_t *timers;
+    Interruptions_t *intpt;
+} Mcu8051_t;
+
+#endif
