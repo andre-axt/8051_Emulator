b368ff20a38393cd55054278e7fbc628ffab6836
andre-axt <andreaxtbachtold@gmail.com>
2026-07-09 15:45:47 -0300

    Add - Define PSW and PCON masks and memory addresses
    Added bitmask definitions for PSW and PCON registers along with addresses for various memory locations.

--- src/memory/em8051_memory.h
+++ src/memory/em8051_memory.h
@@ -8,6 +8,43 @@
 #define SFR_SIZE 128
 #define TOTAL_ADDRESS_SPACE 65536
 
+#define PSW_CY_MASK (1 << 7)
+#define PSW_AC_MASK (1 << 6)
+#define PSW_F0_MASK (1 << 5)
+#define PSW_RS1_MASK (1 << 4)
+#define PSW_RS0_MASK (1 << 3)
+#define PSW_OV_MASK (1 << 2)
+#define PSW_UD_MASK (1 << 1)
+#define PSW_P_MASK (1 << 0)
+
+#define PCON_SMOD_MASK (1 << 7)
+#define PCON_GF1_MASK (1 << 3)
+#define PCON_GF0_MASK (1 << 2)
+#define PCON_PD_MASK (1 << 1)
+#define PCON_IDL_MASK (1 << 0)
+
+#define P0_ADDR 0x80
+#define SP_ADDR 0x81
+#define DPL_ADDR 0x82
+#define DPH_ADDR 0x83
+#define PCON_ADDR 0x87
+#define TCON_ADDR 0x88
+#define TMOD_ADDR 0x89
+#define TL0_ADDR 0x8A
+#define TL1_ADDR 0x8B
+#define TH0_ADDR 0x8C
+#define TH1_ADDR 0x8D
+#define P1_ADDR 0x90
+#define SCON_ADDR 0x98
+#define SBUF_ADDR 0x99
+#define P2_ADDR 0xA0
+#define IE_ADDR 0xA8
+#define P3_ADDR 0xB0
+#define IP_ADDR 0xB8
+#define PSW_ADDR 0xD0
+#define ACC_ADDR 0xE0
+#define B_ADDR 0xF0
+
 typedef struct {
 	uint8_t raw[INTERNAL_RAM_SIZE];
 
