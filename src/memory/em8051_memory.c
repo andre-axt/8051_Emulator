6821d2960e8183658fbe4622fd005c732a444f57
andre-axt <andreaxtbachtold@gmail.com>
2026-07-10 11:04:58 -0300

    Replace - magic numbers with defined addresses for SFRs

--- src/memory/em8051_memory.c
+++ src/memory/em8051_memory.c
@@ -53,32 +53,32 @@ uint8_t memory_read_code (Memory_t *mem, uint16_t address) {
 static uint8_t memory_read_sfr(Memory_t *mem, uint8_t address) {
 	switch (address) {
         
-		case 0x80: return mem->sfr.P0;
-        	case 0x90: return mem->sfr.P1;
-        	case 0xA0: return mem->sfr.P2;
-        	case 0xB0: return mem->sfr.P3;
+		case P0_ADDR:   return mem->sfr.P0;
+		case P1_ADDR:   return mem->sfr.P1;
+		case P2_ADDR:   return mem->sfr.P2;
+		case P3_ADDR:   return mem->sfr.P3;
         
-        	case 0xE0: return mem->sfr.ACC;
-        	case 0xF0: return mem->sfr.B;
-        	case 0x82: return mem->sfr.DPL;
-        	case 0x83: return mem->sfr.DPH;
-        	case 0xA8: return mem->sfr.IE;
-        	case 0xB8: return mem->sfr.IP;
-        	case 0x87: return mem->sfr.PCON;
-        	case 0xD0: return mem->sfr.PSW;
-        	case 0x99: return mem->sfr.SBUF;
-        	case 0x98: return mem->sfr.SCON;
-        	case 0x81: return mem->sfr.SP;
+		case ACC_ADDR:  return mem->sfr.ACC;
+		case B_ADDR:    return mem->sfr.B;
+		case DPL_ADDR:  return mem->sfr.DPL;
+		case DPH_ADDR:  return mem->sfr.DPH;
+		case IE_ADDR:   return mem->sfr.IE;
+		case IP_ADDR:   return mem->sfr.IP;
+		case PCON_ADDR: return mem->sfr.PCON;
+		case PSW_ADDR:  return mem->sfr.PSW;
+		case SBUF_ADDR: return mem->sfr.SBUF;
+		case SCON_ADDR: return mem->sfr.SCON;
+		case SP_ADDR:   return mem->sfr.SP;
         
-        	case 0x88: return mem->sfr.TCON;
-        	case 0x89: return mem->sfr.TMOD;
-        	case 0x8A: return mem->sfr.TL0;
-        	case 0x8B: return mem->sfr.TL1;
-       	 	case 0x8C: return mem->sfr.TH0;
-        	case 0x8D: return mem->sfr.TH1;
+		case TCON_ADDR: return mem->sfr.TCON;
+		case TMOD_ADDR: return mem->sfr.TMOD;
+		case TL0_ADDR:  return mem->sfr.TL0;
+		case TL1_ADDR:  return mem->sfr.TL1;
+		case TH0_ADDR:  return mem->sfr.TH0;
+		case TH1_ADDR:  return mem->sfr.TH1;
         
-        	default: return 0xFF;
-    	}
+		default: return 0xFF;
+    }
 }
 
 uint8_t memory_read_data (Memory_t *mem, uint8_t address) {
@@ -93,32 +93,32 @@ uint8_t memory_read_data (Memory_t *mem, uint8_t address) {
 
 static void memory_write_sfr (Memory_t *mem, uint8_t address, uint8_t value) {
 	switch (address) {
-        	case 0x80: mem->sfr.P0 = value; break;
-        	case 0x90: mem->sfr.P1 = value; break;
-        	case 0xA0: mem->sfr.P2 = value; break;
-        	case 0xB0: mem->sfr.P3 = value; break;
+		case P0_ADDR:   mem->sfr.P0 = value; break;
+		case P1_ADDR:   mem->sfr.P1 = value; break;
+		case P2_ADDR:   mem->sfr.P2 = value; break;
+		case P3_ADDR:   mem->sfr.P3 = value; break;
         	
-		case 0xE0: mem->sfr.ACC = value; break;
-        	case 0xF0: mem->sfr.B = value; break;
-        	case 0x82: mem->sfr.DPL = value; break;
-        	case 0x83: mem->sfr.DPH = value; break;
-        	case 0xA8: mem->sfr.IE = value; break;
-       		case 0xB8: mem->sfr.IP = value; break;
-        	case 0x87: mem->sfr.PCON = value; break;
-        	case 0xD0: mem->sfr.PSW = value; break;
-        	case 0x99: mem->sfr.SBUF = value; break;
-        	case 0x98: mem->sfr.SCON = value; break;
-        	case 0x81: mem->sfr.SP = value; break;
+		case ACC_ADDR:  mem->sfr.ACC = value; break;
+		case B_ADDR:    mem->sfr.B = value; break;
+		case DPL_ADDR:  mem->sfr.DPL = value; break;
+		case DPH_ADDR:  mem->sfr.DPH = value; break;
+		case IE_ADDR:   mem->sfr.IE = value; break;
+		case IP_ADDR:   mem->sfr.IP = value; break;
+		case PCON_ADDR: mem->sfr.PCON = value; break;
+		case PSW_ADDR:  mem->sfr.PSW = value; break;
+		case SBUF_ADDR: mem->sfr.SBUF = value; break;
+		case SCON_ADDR: mem->sfr.SCON = value; break;
+		case SP_ADDR:   mem->sfr.SP = value; break;
         
-		case 0x88: mem->sfr.TCON = value; break;
-        	case 0x89: mem->sfr.TMOD = value; break;
-        	case 0x8A: mem->sfr.TL0 = value; break;
-        	case 0x8B: mem->sfr.TL1 = value; break;
-        	case 0x8C: mem->sfr.TH0 = value; break;
-        	case 0x8D: mem->sfr.TH1 = value; break;
+		case TCON_ADDR: mem->sfr.TCON = value; break;
+		case TMOD_ADDR: mem->sfr.TMOD = value; break;
+		case TL0_ADDR:  mem->sfr.TL0 = value; break;
+		case TL1_ADDR:  mem->sfr.TL1 = value; break;
+		case TH0_ADDR:  mem->sfr.TH0 = value; break;
+		case TH1_ADDR:  mem->sfr.TH1 = value; break;
         
-        	default: break;
-    	}
+		default: break;
+    }
 
 }
 
