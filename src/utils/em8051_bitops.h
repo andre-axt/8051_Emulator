59e598cda618e8f45b78dd6757a337306cd866bb
andre-axt <andreaxtbachtold@gmail.com>
2026-07-13 15:03:41 -0300

    Change - return type of get_bit to uint8_t

--- src/utils/em8051_bitops.h
+++ src/utils/em8051_bitops.h
@@ -5,7 +5,7 @@
 
 typedef struct Mcu8051_t Mcu8051_t;
 
-int8_t get_bit(Mcu8051_t *mcu, uint8_t bit_addr);
+uint8_t get_bit(Mcu8051_t *mcu, uint8_t bit_addr);
 void set_bit(Mcu8051_t *mcu, uint8_t bit_addr, uint8_t value);
 uint8_t fetch_byte(Mcu8051_t *mcu);
 uint16_t fetch_word(Mcu8051_t *mcu);
